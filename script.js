9e966fab4dfec3908b8c6182d2a88450be415500
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-02-28 17:24:24 -0300

    Hover effect made!

--- script.js
+++ script.js
@@ -2,17 +2,37 @@ let frame = document.querySelector('.frame')
 
 function workFrameGenerator(number){
     for(let i=1;i<=number;i++){
-        let unit = document.createElement(`div${i}`);
-        unit.classList.add('unit');
+        let unit = document.createElement('div');
+        const Nfather= document.createAttribute("class")
+        Nfather.value = `father${i}`;
         frame.appendChild(unit);
-        for(let i=1;i<=number;i++){
-            let sonsUnit=document.createElement('div')
-            sonsUnit.classList.add('sonsUnit')
-            unit.appendChild(sonsUnit)
+        unit.setAttributeNode(Nfather);
+        unit.classList.add('unit');
+       
+        for(let j=1;j<=number;j++){
+            let sonsUnit=document.createElement('div');
+            unit.appendChild(sonsUnit);
+            const Nson= document.createAttribute("class");
+            Nson.value=`son${j}`;
+            sonsUnit.setAttributeNode(Nson);
+            sonsUnit.classList.add('sonsUnit');
         }
 
        
     }
+     makeTheSonsReady(number);
 
 };
-workFrameGenerator(32);
+
+
+function makeTheSonsReady(number){
+    for(let i=1;i<=number;i++){
+      let father = frame.querySelector(`.father${i}`);
+      for(let j=1;j<=number;j++){
+        let son = father.querySelector(`.son${j}`)
+        son.addEventListener("mouseover",() =>{son.style.backgroundColor="Black"})
+      
+      
+    }
+};}
+workFrameGenerator(16);
